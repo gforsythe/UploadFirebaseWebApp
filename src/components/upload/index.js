3bdddbb0b62cea6d77add2ad94437a6ec940fa3d
esg-gforsythe <84102314+esg-gforsythe@users.noreply.github.com>
2022-04-28 16:01:42 -0700

    custom meta data added to uploading a file

--- src/components/upload/index.js
+++ src/components/upload/index.js
@@ -1,20 +1,27 @@
 import React, { useState, useRef } from "react";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
-import { storage } from "../../Utils/firebase";
+import { auth, storage, db,   } from "../../Utils/firebase";
+import { doc, updateDoc, } from "firebase/firestore";
 
 function Upload() {
   const [progress, setProgress] = useState(0);
   const [selectedFile, setSelectedFile] = useState();
   const pauseRef = useRef();
   const resumeRef = useRef();
-  const cancelRef = useRef();
+  const cancelRef = useRef(); 
 
   const handleUpload = (e) => {
     e.preventDefault();
     console.log("Clicked");
     let fileRef = ref(storage, `images/${selectedFile.name}`);
-    
-    const uploadTask = uploadBytesResumable(fileRef, selectedFile);
+    const metadata = {
+      customMetadata: {
+        "PLEASE": "WORK",
+        "electric": "so cool!",
+      }
+     
+    }
+    const uploadTask = uploadBytesResumable(fileRef, selectedFile, metadata);
     
 
     uploadTask.on(
@@ -49,7 +56,12 @@ function Upload() {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl)=>{
           console.log(`Check out the file ${downloadUrl}`);
         })
-     
+        
+        let getUser = auth.currentUser
+        let docRef = doc(db, 'users', getUser.uid)
+        updateDoc(docRef,{
+          image: uploadTask.snapshot.ref.name,  
+        })
       }
     );
 
@@ -89,7 +101,9 @@ function Upload() {
       <div className="form-group">
       <button className="btn btn-warning" ref={pauseRef}>PAUSE</button>
       <br/>
+      <br/>
       <button className="btn btn-success" ref={resumeRef}>RESUME</button>
+      <br/>
       <br/>
       <button className="btn btn-danger" ref={cancelRef}>CANCEL</button>
 
