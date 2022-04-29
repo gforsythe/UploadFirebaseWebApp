ccd15106d71c7bed21feee86dc567b0342ca5daa
esg-gforsythe <84102314+esg-gforsythe@users.noreply.github.com>
2022-04-29 16:50:24 -0700

    added a mapped out version of the items in a list with a corresponding anchor tag, will update later with a Link tag via react router and properly delete button

--- src/components/upload/list.js
+++ src/components/upload/list.js
@@ -1,41 +1,52 @@
-import React, {useState, useEffect } from 'react';
-import { storage, } from '../../Utils/firebase';
-import {ref, listAll} from 'firebase/storage';
+import React, { useState, useEffect } from "react";
+import { storage } from "../../Utils/firebase";
+import { ref, listAll, getDownloadURL, deleteObject } from "firebase/storage";
 function ListUploads() {
-const [image, setImage] = useState([])
-const listRef = ref(storage, 'images');
-    useEffect(() => {
-     grabImages()
-    
-      return () => {
-        
-      };
-    }, [])
+  const [image, setImage] = useState([]);
+  const listRef = ref(storage, "images");
+  useEffect(() => {
+    grabImages();
 
-    function grabImages() {
-        listAll(listRef)
-        .then((res) =>{
-            
-            res.items.forEach((itemRef) =>{
-               image.push(itemRef)
-        
-            })
-            setImage(image);
-        
-        }).catch((error)=>{
-            console.log(error);
-        })
-    }
+    return () => {};
+  }, [image]);
 
-    
+  function grabImages() {
+    listAll(listRef)
+      .then((res) => {
+        res.items.forEach((itemRef) => {
+          getDownloadURL(itemRef).then((downloadURL) => {
+            image.push({
+              name: itemRef.name,
+              link: downloadURL,
+            });
+          });
+        });
+        setImage(image);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function handleDelete(){
+  
+  }
 
   return (
     <ul>
-    {image.map((item, i) =>(
-        <li key={i}>{item.name}</li>
-    )) 
-    }</ul>
-  )
+      {image.map((item, i) => (
+        <li key={i}>
+          {item.name}
+          <br/>
+          <a className="btn btn-info" href={item.link}>Open me</a>
+          <br/>
+          <button onClick={() => handleDelete(i)} className="btn btn-warning">DELETE</button>
+          <br/>
+          <br/>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default ListUploads;
